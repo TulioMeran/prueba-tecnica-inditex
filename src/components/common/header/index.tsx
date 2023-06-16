d758e1fbe1e6437d7dccce1646c3b8f8fbe67580
Tulio Meran <rtulio@arssenasa.gob.do>
2023-06-15 23:22:40 -0400

    header navegando a inicio

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,9 +1,13 @@
 import { Paper, Typography } from "@mui/material"
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+    let navigate = useNavigate()
     return (
         <Paper sx={{ paddingY: 1 }} >
-            <Typography sx={{ fontWeight: '600', fontSize: 20, color: '#17B1DE' }} >Podcaster</Typography>
+            <Typography sx={{ fontWeight: '600', fontSize: 20, color: '#17B1DE' }} onClick={() => {
+                navigate("/")
+            }} >Podcaster</Typography>
         </Paper>
     )
 }
